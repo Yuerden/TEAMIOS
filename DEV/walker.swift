23a33dad2b10eaa76d1123fec9462254d04b044f
Yuerden <98784955+Yuerden@users.noreply.github.com>
2022-03-24 19:19:55 -0400

    Update walker.swift

--- DEV/walker.swift
+++ DEV/walker.swift
@@ -2,10 +2,15 @@ class walker: Enemy {
     //standard speed, health
     #define MAX_HEALTH 100
     #define SPEED 3
+    #define ID 0
+
+    override var speed: Int  { get { return SPEED } }
+    override var hpMax: Int  { get { return MAX_HEALTH } }
+    override var ID: Int     { get { return ID } }
 
     override init() { init(x: 0, y: 0) }
 
     override init(x: Int, y: Int){
-        super.init(x: x, y: y, speed: SPEED, hpMax: MAX_HEALTH)
+        super.init(x: x, y: y)
     }
 }
