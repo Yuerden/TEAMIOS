5bece0b1570995c2a958ee9e7493ffb3e0c8d192
Yuerden <98784955+Yuerden@users.noreply.github.com>
2022-03-24 19:19:43 -0400

    Update tank.swift

--- DEV/tank.swift
+++ DEV/tank.swift
@@ -2,7 +2,12 @@ class tank: Enemy {
     //slow, high health
     #define MAX_HEALTH 250
     #define SPEED 1
+    #define ID 1
 
+    override var speed: Int  { get { return SPEED } }
+    override var hpMax: Int  { get { return MAX_HEALTH } }
+    override var ID: Int     { get { return ID } }
+    
     override init() { init(x: 0, y: 0) }
 
     override init(x: Int, y: Int){
